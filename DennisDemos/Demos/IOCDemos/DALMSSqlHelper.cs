1589ad563b2c5f8170a6cf231ca27db92588bb68
15040554157@163.com <15040554157@163.com>
2018-06-03 14:30:16 +0800

    IOC demo

---
+++ DennisDemos/Demos/IOCDemos/DALMSSqlHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisDemos.Demos.IOCDemos
+{
+    public class DALMSSqlHelper : ISqlHelper
+    {
+        public int Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Query()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
