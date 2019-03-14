f923521b34eab0c98002c8a919e253c56ba746e7
dennis wu <15040554157@163.com>
2019-03-14 20:28:29 +0800

    ad

--- DennisDemos/Demoes/OtherDemo.cs
+++ DennisDemos/Demoes/OtherDemo.cs
@@ -11,7 +11,15 @@ namespace DennisDemos.Demoes
     {
         public override void Run()
         {
-            var wbFeature4AdsFedPStr = Regex.Replace("{\"2785\":\"50\",\"940\":\"19\",\"942\":\"50\"}", @"[\""\{\}]", "");
+            //ModelName[ \t]*=[ \t]*Woodblocks(?<FormatVersion>V\\d)?\\.US\\.Slot(?<SlotId>[0-9]+)
+            Regex test = new Regex("{\"FeatureId\":\"(?<FeatureId>[0-9]+)\",\"SinkNodeId\":\"[0-9]+\",\"SlotId\":\"[0-9]+\"}", RegexOptions.Compiled);
+            string target = "[{\"FeatureId\":\"941\",\"SinkNodeId\":\"0\",\"SlotId\":\"51\"},{\"FeatureId\":\"909\",\"SinkNodeId\":\"1\",\"SlotId\":\"51\"}]";
+            var hits = test.Matches(target);
+            if (hits.Count>0)
+            {
+               var res = hits[0].Groups["FeatureId"].Value;
+            }
+            var wbFeature4AdsFedPStr = Regex.Replace("[{\"FeatureId\":\"941\",\"SinkNodeId\":\"0\",\"SlotId\":\"51\"},{\"FeatureId\":\"909\",\"SinkNodeId\":\"1\",\"SlotId\":\"51\"}]", @"[\""\{\}\[\]]", "");
             var featureModels = wbFeature4AdsFedPStr.Split(new char[] { ',' },
                 StringSplitOptions.RemoveEmptyEntries);
             foreach (var featureModel in featureModels)
