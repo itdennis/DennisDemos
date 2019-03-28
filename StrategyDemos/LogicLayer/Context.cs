6665545764ff8627e45119e7361a810129dd2d1b
dennis wu <15040554157@163.com>
2019-03-28 15:26:24 +0800

    add strategy demo

---
+++ StrategyDemos/LogicLayer/Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.LogicLayer
+{
+    public class Context
+    {
+        AlgorithmSuper algorithmSuper;
+        public Context(string type)
+        {
+            switch (type)
+            {
+                case "normal":
+                    this.algorithmSuper = new CashNomal();
+                    break;
+                case "rebate 0.8":
+                    this.algorithmSuper = new CashRebate(0.8);
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        public double GetResult(double money)
+        {
+            return this.algorithmSuper.GetResult(money);
+        }
+    }
+}
---
+++ StrategyDemos/Program.cs
@@ -0,0 +1,21 @@
+﻿using Strategy.LogicLayer;
+using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        /// <summary>
+        /// 这种方式对于客户端来说, 只暴露了context类, 对于算法核心部分根本不用了解.
+        /// 很好的解耦了client, context, algorithm三个类.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            Context context = new Context("normal");
+            
+            Console.WriteLine(context.GetResult(1000));
+        }
+    }
+}
