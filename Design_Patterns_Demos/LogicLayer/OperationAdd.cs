a691a467009e82f4eda1524bdf596d1f2317a6dc
dennis wu <15040554157@163.com>
2019-03-28 14:15:47 +0800

    add simple factory.

---
+++ Design_Patterns_Demos/LogicLayer/OperationAdd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleFactory.LogicLayer
+{
+    public class OperationAdd: Operation
+    {
+        public override double GetResult()
+        {
+            return NumberA + NumberB;
+        }
+    }
+}
---
+++ Design_Patterns_Demos/LogicLayer/OperationFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleFactory.LogicLayer
+{
+    public class OperationFactory
+    {
+        public static Operation CreateOperation(string op)
+        {
+            Operation operation = null;
+            switch (op)
+            {
+                case "+":
+                    operation = new OperationAdd();
+                    break;
+                case "-":
+                    operation = new OperationSub();
+                    break;
+                default:
+                    break;
+            }
+            return operation;
+        }
+    }
+}
