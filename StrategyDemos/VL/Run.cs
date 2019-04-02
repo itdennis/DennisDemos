a98652b12885a4c4b8c594a5300e0de1f06f2fb5
dennis wu <15040554157@163.com>
2019-04-02 11:53:48 +0800

    update strategy demo

--- StrategyDemos/VL/Run.cs
+++ StrategyDemos/VL/Run.cs
@@ -3,17 +3,19 @@ using System;
 
 namespace Strategy
 {
-    class Program
+    public class Run
     {
         /// <summary>
         /// 这种方式对于客户端来说, 只暴露了context类, 对于算法核心部分根本不用了解.
         /// 很好的解耦了client, context, algorithm三个类.
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
+        public static void Runner()
         {
-
-            Run.Runner();
+            
+            Context context = new Context("normal");
+            
+            Console.WriteLine(context.GetResult(1000));
         }
     }
 }
