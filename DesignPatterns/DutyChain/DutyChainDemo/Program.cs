c645a3c945133f31a4643017fd24f9ee7743c3c3
dennis wu <15040554157@163.com>
2019-04-17 23:49:37 +0800

    add duty chain at 2019-4-17 23:49:35

---
+++ DesignPatterns/DutyChain/DutyChainDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DutyChainDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
