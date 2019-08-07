7b50c03af9f22625fa145e374b15e22cd244a650
dennis wu <15040554157@163.com>
2019-08-07 18:32:59 +0800

    update at 2019-8-7 18:32:57

--- WpfApp1_demo/WpfApp1_demo/App.xaml.cs
+++ WpfApp1_demo/WpfApp1_demo/App.xaml.cs
@@ -13,5 +13,14 @@ namespace WpfApp1_demo
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+
+            if (e.Args.Length == 1)
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+
+            wnd.Show();
+        }
     }
 }
