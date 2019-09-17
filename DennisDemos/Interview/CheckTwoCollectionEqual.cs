7d234dee35e0a9813d5860ee38f4bb1124e08a8b
dennis wu <15040554157@163.com>
2019-09-17 18:41:04 +0800

    update at 2019-9-17 18:41:03

---
+++ DennisDemos/Interview/CheckTwoCollectionEqual.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisDemos.Interview
+{
+    public class CheckTwoCollectionEqual
+    {
+        #region Demo
+        static void Demo()
+        {
+            var now = DateTimeOffset.UtcNow.DateTime.ToShortDateString();
+            List<string> lA = new List<string>() { "111", "222", "222", "333" };
+            List<string> lB = new List<string>() { "111", "222", "333", "333" };
+            if (ScrambledEquals(lA, lB))
+            {
+                Console.WriteLine($"la and lb is ScrambledEquals equal.");
+            }
+            if (lA.All(lB.Contains))
+            {
+                Console.WriteLine($"la and lb is All equal.");
+            }
+            if (lA.SequenceEqual(lB))
+            {
+                Console.WriteLine($"la and lb is SequenceEqual.");
+            }
+
+            Dictionary<string, string> dA = new Dictionary<string, string>() { { "111", "value" }, { "222", "value" }, { "333", "value" }, { "444", "value" }, { "555", "value" } };
+            Dictionary<string, string> dB = new Dictionary<string, string>() { { "111", "value" }, { "222", "value" }, { "333", "value" }, { "444", "value" }, { "555", "value" } };
+
+            if (dA.SequenceEqual(dB))
+            {
+                Console.WriteLine($"da and db is equal.");
+            }
+        }
+
+        public static bool ScrambledEquals<T>(IEnumerable<T> list1, IEnumerable<T> list2)
+        {
+            var cnt = new Dictionary<T, int>();
+            foreach (T s in list1)
+            {
+                if (cnt.ContainsKey(s))
+                {
+                    cnt[s]++;
+                }
+                else
+                {
+                    cnt.Add(s, 1);
+                }
+            }
+            foreach (T s in list2)
+            {
+                if (cnt.ContainsKey(s))
+                {
+                    cnt[s]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return cnt.Values.All(c => c == 0);
+        }
+
+
+        public static bool ScrambledEquals<T>(IEnumerable<T> list1, IEnumerable<T> list2, IEqualityComparer<T> comparer)
+        {
+            var cnt = new Dictionary<T, int>(comparer);
+            return false;
+        }
+        #endregion
+
+        
+    }
+}
---
+++ DennisDemos/Interview/IEqualityComparer_demo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisDemos.Interview
+{
+    public class IEqualityComparer_demo
+    {
+        #region Run
+        public static void Run()
+        {
+            BoxEqualityComparer boxEqC = new BoxEqualityComparer();
+
+            var boxes = new Dictionary<Box, string>(boxEqC);
+
+            var redBox = new Box(4, 3, 4);
+            AddBox(boxes, redBox, "red");
+
+            var blueBox = new Box(4, 3, 4);
+            AddBox(boxes, blueBox, "blue");
+
+            var greenBox = new Box(3, 4, 3);
+            AddBox(boxes, greenBox, "green");
+            Console.WriteLine();
+
+            Console.WriteLine("The dictionary contains {0} Box objects.",
+                              boxes.Count);
+        }
+        private static void AddBox(Dictionary<Box, String> dict, Box box, String name)
+        {
+            try
+            {
+                dict.Add(box, name);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Unable to add {0}: {1}", box, e.Message);
+            }
+        }
+        #endregion
+    }
+
+    public class Box
+    {
+        public Box(int h, int l, int w)
+        {
+            this.Height = h;
+            this.Length = l;
+            this.Width = w;
+        }
+
+        public int Height { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public override String ToString()
+        {
+            return String.Format("({0}, {1}, {2})", Height, Length, Width);
+        }
+    }
+
+    class BoxEqualityComparer : IEqualityComparer<Box>
+    {
+        public bool Equals(Box b1, Box b2)
+        {
+            if (b2 == null && b1 == null)
+                return true;
+            else if (b1 == null || b2 == null)
+                return false;
+            else if (b1.Height == b2.Height && b1.Length == b2.Length
+                                && b1.Width == b2.Width)
+                return true;
+            else
+                return false;
+        }
+
+        public int GetHashCode(Box bx)
+        {
+            int hCode = bx.Height ^ bx.Length ^ bx.Width;
+            return hCode.GetHashCode();
+        }
+    }
+}
