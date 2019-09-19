d31f943a3684f2df0f9ff732809879a0d93b99d7
dennis wu <15040554157@163.com>
2019-09-19 14:27:00 +0800

    update at 2019-9-19 14:26:58

--- DennisDemos/Program.cs
+++ DennisDemos/Program.cs
@@ -10,10 +10,39 @@ namespace DennisDemos
 {
     class Program
     {
+        private enum TimeComparison
+        {
+            Earlier = -1,
+            Same = 0,
+            Later = 1
+        };
         static void Main(string[] args)
         {
             //CheckTwoCollectionEqual.Run();
             //CheckTwoCollectionEqual.Demo();
+
+            {
+                DateTimeOffset firstTime = new DateTimeOffset(2019, 9, 15, 8, 59, 59,
+                                 new TimeSpan(-7, 0, 0));
+
+                DateTimeOffset secondTime = firstTime;
+                Console.WriteLine("Comparing {0} and {1}: {2}",
+                                  firstTime, secondTime,
+                                  (TimeComparison)firstTime.CompareTo(secondTime));
+
+                secondTime = new DateTimeOffset(2007, 9, 1, 6, 45, 0,
+                                 new TimeSpan(-6, 0, 0));
+                Console.WriteLine("Comparing {0} and {1}: {2}",
+                                 firstTime, secondTime,
+                                 (TimeComparison)firstTime.CompareTo(secondTime));
+
+                secondTime = new DateTimeOffset(2007, 9, 1, 8, 45, 0,
+                                 new TimeSpan(-5, 0, 0));
+                Console.WriteLine("Comparing {0} and {1}: {2}",
+                                 firstTime, secondTime,
+                                 (TimeComparison)firstTime.CompareTo(secondTime));
+            }
+
 
             List<Tuple<DateTime, string>> cache = new List<Tuple<DateTime, string>>();
             for (int i = 0; i < 5; i++)
