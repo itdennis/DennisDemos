b7c0ae04458a1da36e4d27d15dd14f6d853f52b5
dennis wu <15040554157@163.com>
2019-09-22 22:41:02 +0800

    update at 2019-9-22 22:40:59

---
+++ DennisDemos/Demoes/DatetimeOffset.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisDemos.Demoes
+{
+
+    class DatetimeOffset
+    {
+        private enum TimeComparison
+        {
+            Earlier = -1,
+            Same = 0,
+            Later = 1
+        };
+        public void Run()
+        {
+            {
+                DateTimeOffset firstTime = new DateTimeOffset(2019, 9, 15, 8, 59, 59,
+                                 new TimeSpan(-7, 0, 0));
+
+                DateTimeOffset secondTime = firstTime;
+                Console.WriteLine("Comparing {0} and {1}: {2}",
+                                  firstTime, secondTime,
+                                  (TimeComparison)firstTime.CompareTo(secondTime));
+
+                secondTime = new DateTimeOffset(2007, 9, 1, 6, 45, 0,
+                                 new TimeSpan(-6, 0, 0));
+                Console.WriteLine("Comparing {0} and {1}: {2}",
+                                 firstTime, secondTime,
+                                 (TimeComparison)firstTime.CompareTo(secondTime));
+
+                secondTime = new DateTimeOffset(2007, 9, 1, 8, 45, 0,
+                                 new TimeSpan(-5, 0, 0));
+                Console.WriteLine("Comparing {0} and {1}: {2}",
+                                 firstTime, secondTime,
+                                 (TimeComparison)firstTime.CompareTo(secondTime));
+            }
+        }
+    }
+}
--- DennisDemos/Program.cs
+++ DennisDemos/Program.cs
@@ -10,52 +10,24 @@ namespace DennisDemos
 {
     class Program
     {
-        private enum TimeComparison
-        {
-            Earlier = -1,
-            Same = 0,
-            Later = 1
-        };
         static void Main(string[] args)
         {
             //CheckTwoCollectionEqual.Run();
             //CheckTwoCollectionEqual.Demo();
-
+            List<string> lA = new List<string>() { "111", "222", "222", "333" };
+            List<string> lB = new List<string>() { "111", "222", "333", "333" };
+            bool res = lA.All(lB.Contains) && lA.Count == lB.Count;
+            if (res)
             {
-                DateTimeOffset firstTime = new DateTimeOffset(2019, 9, 15, 8, 59, 59,
-                                 new TimeSpan(-7, 0, 0));
-
-                DateTimeOffset secondTime = firstTime;
-                Console.WriteLine("Comparing {0} and {1}: {2}",
-                                  firstTime, secondTime,
-                                  (TimeComparison)firstTime.CompareTo(secondTime));
-
-                secondTime = new DateTimeOffset(2007, 9, 1, 6, 45, 0,
-                                 new TimeSpan(-6, 0, 0));
-                Console.WriteLine("Comparing {0} and {1}: {2}",
-                                 firstTime, secondTime,
-                                 (TimeComparison)firstTime.CompareTo(secondTime));
-
-                secondTime = new DateTimeOffset(2007, 9, 1, 8, 45, 0,
-                                 new TimeSpan(-5, 0, 0));
-                Console.WriteLine("Comparing {0} and {1}: {2}",
-                                 firstTime, secondTime,
-                                 (TimeComparison)firstTime.CompareTo(secondTime));
+                Console.WriteLine("完全一致");
+            }
+            else
+            {
+                Console.WriteLine("不完全一致");
             }
 
 
-            List<Tuple<DateTime, string>> cache = new List<Tuple<DateTime, string>>();
-            for (int i = 0; i < 5; i++)
-            {
-                var date = DateTime.Now.AddDays(i);
-                cache.Add(new Tuple<DateTime, string>(date, date.ToString("yyyy-MM-dd")));
-            }
-            cache = cache.OrderByDescending(c => c.Item1).ToList();
 
-            var actuallyResults = cache.Select(a => a.Item1.ToString("yyyy-MM-dd")).ToList();
         }
-
-
-        
     }
 }
