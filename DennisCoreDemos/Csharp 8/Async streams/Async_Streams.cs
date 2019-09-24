2c9ae969f0bb7e5fb0472e59f970b79dca36a896
dennis wu <15040554157@163.com>
2019-09-24 18:25:50 +0800

    update at 2019-9-24 18:25:47

---
+++ DennisCoreDemos/Csharp 8/Async streams/Async_Streams.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisCoreDemos.Csharp_8.Async_streams
+{
+    class Async_Streams
+    {
+        async IAsyncEnumerable<int> GetBigResultsAsync()
+        {
+            await foreach (var result in GetResultsAsync())
+            {
+                if (result > 20) yield return result;
+            }
+        }
+
+        private async Task<IEnumerable<object>> GetResultsAsync()
+        {
+            //Task<string[]> result = await File.ReadAllLinesAsync(@"C:\Users\v-yanywu\Desktop\dennis\pic\test.txt", Encoding.UTF8);
+            //return result;
+            var res = File.ReadAllLines(@"C:\Users\v-yanywu\Desktop\dennis\pic\test.txt", Encoding.UTF8);
+            return res.
+        }
+    }
+}
