d0a01da5e4abced3b1d9f5609cc29458d4fb1edc
dennis wu <15040554157@163.com>
2019-09-25 18:53:56 +0800

    update at 2019-9-25 18:53:55

---
+++ Core3.0WebApp/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core3._0WebApp.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string PostContent { get; set; }
+        public bool CommentEnabled { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreateOnUtc { get; set; }
+        public string ContentAbstract { get; set; }
+    }
+}
