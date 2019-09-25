f63119c70248c27ef20d59ca76ac7ad9941c9b6b
dennis wu <15040554157@163.com>
2019-09-25 18:28:13 +0800

    update at 2019-9-25 18:28:11

---
+++ Core3.0WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core3._0WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
