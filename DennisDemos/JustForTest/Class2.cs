312dbd695e24d3d0702941583829b1739ad0ffba
dennis wu <15040554157@163.com>
2019-10-24 17:35:52 +0800

    update at 2019-10-24 17:35:50

---
+++ DennisDemos/JustForTest/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisDemos.JustForTest
+{
+    public class Class2 : Class1
+    {
+
+        public void Run() 
+        {
+            var a = base.MyProperty;
+        }
+    }
+}
