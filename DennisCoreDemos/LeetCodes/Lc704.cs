be0aaac27d37b223ab329e03f3c6b8cfc1405c61
dennis wu <15040554157@163.com>
2019-11-06 18:23:37 +0800

    update at 2019-11-6 18:23:35

---
+++ DennisCoreDemos/LeetCodes/Lc704.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisCoreDemos.LeetCodes
+{
+    public class Lc704
+    {
+        public static async Task<int> Run(int[] nums, int target)
+        {
+            Task<int> task = new Task<int>(() => { return DoOps(nums, target); });
+            task.Start();
+            return await task;
+        }
+
+        private static int DoOps(int[] nums, int target)
+        {
+            int len = nums.Length;
+            if (len == 0)
+            {
+                return len;
+            }
+            int left = 0;
+            int right = len -1 ;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                    right = mid;
+            }
+            if (nums[left] == target)
+            {
+                return left;
+            }
+            else
+                return -1;
+        }
+    }
+}
--- DennisCoreDemos/Program.cs
+++ DennisCoreDemos/Program.cs
@@ -17,7 +17,8 @@ namespace DennisCoreDemos
             //await Lc15.Run("[");
             //await Lc27.Run(new int[] { 1,2,3,2,4 }, 2);
             //await Lc28.Run("hello", "ll");
-            Console.WriteLine(await Lc35.Run(new int[] { 1, 3, 5 }, 3));
+            //Console.WriteLine(await Lc35.Run(new int[] { 1, 3, 5 }, 3));
+            Console.WriteLine(await Lc704.Run(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
 
             Console.ReadKey();
         }
