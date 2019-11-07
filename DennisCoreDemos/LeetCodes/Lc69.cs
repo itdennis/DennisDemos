2a05dbf304c673e555df90701b19841fc5d7c564
dennis wu <15040554157@163.com>
2019-11-07 18:26:56 +0800

    update at 2019-11-7 18:26:54

---
+++ DennisCoreDemos/LeetCodes/Lc69.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DennisCoreDemos.LeetCodes
+{
+    public class Lc69
+    {
+        public static async Task<int> Run(int x)
+        {
+            Task<int> task = new Task<int>(() => { return DoOps(x); });
+            task.Start();
+            return await task;
+        }
+
+        private static int DoOps(int x)
+        {
+            int left = 0;
+            int right = x;
+            while (left < right)
+            {
+                int mid = left + (right - left + 1) / 2;
+                long squre = mid * mid;
+                if (squre > x)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+            return left;
+        }
+    }
+}
--- DennisCoreDemos/Program.cs
+++ DennisCoreDemos/Program.cs
@@ -18,8 +18,8 @@ namespace DennisCoreDemos
             //await Lc27.Run(new int[] { 1,2,3,2,4 }, 2);
             //await Lc28.Run("hello", "ll");
             //Console.WriteLine(await Lc35.Run(new int[] { 1, 3, 5 }, 3));
-            Console.WriteLine(await Lc704.Run(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
-
+            //Console.WriteLine(await Lc704.Run(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
+            Console.WriteLine(await Lc69.Run(2147395599));
             Console.ReadKey();
         }
     }
