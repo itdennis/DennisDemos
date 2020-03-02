8db7031babfc182ab187f0b07cc3b694d8f53b9d
dennis wu <15040554157@163.com>
2020-03-02 21:56:45 +0800

    update at 2020-3-2 21:56:42

--- ocelot-demo/Ocelot.Core/Startup.cs
+++ ocelot-demo/Ocelot.Core/Startup.cs
@@ -7,16 +7,33 @@ using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
 using Ocelot.Provider.Polly;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using IdentityServer4.AccessTokenValidation;
 
 namespace Ocelot.Core
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOcelot().AddConsul().AddPolly();
             var authenticationProviderKey = "TestKey";
-            services.AddAuthentication().AddJwtBearer(authenticationProviderKey, op => { });
+
+            Action<IdentityServerAuthenticationOptions> isaOpt = option =>
+            {
+                option.Authority = _config["IdentityService:Uri"];
+                option.ApiName = "clientservice";
+                option.RequireHttpsMetadata = Convert.ToBoolean(_config["IdentityService:UseHttps"]);
+                option.SupportedTokens = SupportedTokens.Both;
+                option.ApiSecret = _config["IdentityService:ApiSecrets:clientservice"];
+            };
+            services.AddAuthentication().AddIdentityServerAuthentication(authenticationProviderKey, isaOpt);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
